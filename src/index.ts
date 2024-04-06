3c1e65c079e62c04e0b1c59bcb21dd86ab3f86e4
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2024-04-06 19:33:24 -0400

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,26 @@ import 'dotenv/config';
 import { exec } from 'node:child_process';
 import { createWriteStream } from 'node:fs';
 import { join } from 'node:path';
-import { setTimeout as delay } from 'node:timers/promises';
 
 const path = process.argv[2];
-const intents = [
-    513,//guilds and guildMessages
-    98045,//all except privileged
-];
-const numShards = 3;
+const intents = process.argv[3] === 'light'
+    ? 513//guilds and guildMessages
+    : 98045;//all except privileged
+console.log({ intents });
 const end = Date.now() + (60 * 60 * 1e3);
 (async () => {
-    for (let i of intents) {
-        const stream = createWriteStream(join(process.cwd(), 'results', `${path.split('.js')[0]}-${i}.txt`));
-        const bot = exec(`node ./dist/${path} ${i}`);
+    const stream = createWriteStream(join(process.cwd(), 'results', `${path.split('.js')[0]}-${intents}.txt`));
+    const bot = exec(`node ./dist/${path} ${intents}`);
 
-        bot.stdout?.on('data', (data) => {
-            if (typeof data === 'string' && data.startsWith('{ heapUsed')) {
-                const memoryUsage = JSON.parse(data.replace(/heapUsed|rss|heapTotal/g, val => `"${val}"`));
-                const text = `{ heapUsed: ${memoryUsage.heapUsed}, heapTotal: ${memoryUsage.heapTotal}, rss: ${memoryUsage.rss}, now: ${Date.now()} }\n`;
-                const date = new Date(end - Date.now());
-                console.log(path, { i, heapUsed: formatMemoryUsage(memoryUsage.heapUsed), heapTotal: formatMemoryUsage(memoryUsage.heapTotal), rss: formatMemoryUsage(memoryUsage.rss) }, date.toISOString().substring(14, 19));
-                stream.write(text);
-            } else console.log(i, data);
-        });
-        await delay(numShards * 5.5e3);
-    }
+    bot.stdout?.on('data', (data) => {
+        if (typeof data === 'string' && data.startsWith('{ heapUsed')) {
+            const memoryUsage = JSON.parse(data.replace(/heapUsed|rss|heapTotal/g, val => `"${val}"`));
+            const text = `{ heapUsed: ${memoryUsage.heapUsed}, heapTotal: ${memoryUsage.heapTotal}, rss: ${memoryUsage.rss}, now: ${Date.now()} }\n`;
+            const date = new Date(end - Date.now());
+            console.log(path, { intents, heapUsed: formatMemoryUsage(memoryUsage.heapUsed), heapTotal: formatMemoryUsage(memoryUsage.heapTotal), rss: formatMemoryUsage(memoryUsage.rss) }, date.toISOString().substring(14, 19));
+            stream.write(text);
+        } else console.log(intents, data);
+    });
 })();
 
 setTimeout(() => {
